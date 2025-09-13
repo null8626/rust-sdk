f44105e3f43b32a8d97d3768d80b3c844ea8326d
null8626 <vierofernando9@gmail.com>
2025-09-13 18:52:06 +0700

    *: rewrite v0

--- src/webhooks/actix_web.rs
+++ src/webhooks/actix_web.rs
@@ -1,24 +1,28 @@
-use crate::{IncomingVote, Vote};
+use crate::Incoming;
 use actix_web::{
   dev::Payload,
-  error::{Error, ErrorUnauthorized},
+  error::{Error, ErrorBadRequest, ErrorUnauthorized},
   web::Json,
   FromRequest, HttpRequest,
 };
-use core::{
+use serde::de::DeserializeOwned;
+use std::{
   future::Future,
   pin::Pin,
   task::{ready, Context, Poll},
 };
 
 #[doc(hidden)]
-pub struct IncomingVoteFut {
+pub struct IncomingFut<T: DeserializeOwned> {
   req: HttpRequest,
-  json_fut: <Json<Vote> as FromRequest>::Future,
+  json_fut: <Json<T> as FromRequest>::Future,
 }
 
-impl Future for IncomingVoteFut {
-  type Output = Result<IncomingVote, Error>;
+impl<T> Future for IncomingFut<T>
+where
+  T: DeserializeOwned,
+{
+  type Output = Result<Incoming<T>, Error>;
 
   fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
     if let Ok(json) = ready!(Pin::new(&mut self.json_fut).poll(cx)) {
@@ -26,26 +30,31 @@ impl Future for IncomingVoteFut {
 
       if let Some(authorization) = headers.get("Authorization") {
         if let Ok(authorization) = authorization.to_str() {
-          return Poll::Ready(Ok(IncomingVote {
+          return Poll::Ready(Ok(Incoming {
             authorization: authorization.to_owned(),
-            vote: json.into_inner(),
+            data: json.into_inner(),
           }));
         }
       }
+
+      return Poll::Ready(Err(ErrorUnauthorized("401")));
     }
 
-    Poll::Ready(Err(ErrorUnauthorized("401")))
+    Poll::Ready(Err(ErrorBadRequest("400")))
   }
 }
 
 #[cfg_attr(docsrs, doc(cfg(feature = "actix-web")))]
-impl FromRequest for IncomingVote {
+impl<T> FromRequest for Incoming<T>
+where
+  T: DeserializeOwned,
+{
   type Error = Error;
-  type Future = IncomingVoteFut;
+  type Future = IncomingFut<T>;
 
   #[inline(always)]
   fn from_request(req: &HttpRequest, payload: &mut Payload) -> Self::Future {
-    IncomingVoteFut {
+    IncomingFut {
       req: req.clone(),
       json_fut: Json::from_request(req, payload),
     }
