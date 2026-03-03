b6442c03f2dbc70afe2c49a566c70317bfe44210
null <60427892+null8626@users.noreply.github.com>
2026-03-03 17:25:12 +0700

    style: prettier

--- src/webhooks/actix_web.rs
+++ src/webhooks/actix_web.rs
@@ -1,15 +1,15 @@
 use crate::Incoming;
 use actix_web::{
+  FromRequest, HttpRequest,
   dev::Payload,
   error::{Error, ErrorBadRequest, ErrorUnauthorized},
   web::Json,
-  FromRequest, HttpRequest,
 };
 use serde::de::DeserializeOwned;
 use std::{
   future::Future,
   pin::Pin,
-  task::{ready, Context, Poll},
+  task::{Context, Poll, ready},
 };
 
 #[doc(hidden)]
--- src/webhooks/warp.rs
+++ src/webhooks/warp.rs
@@ -1,7 +1,7 @@
 use super::Webhook;
 use serde::de::DeserializeOwned;
 use std::sync::Arc;
-use warp::{body, header, http::StatusCode, path, Filter, Rejection, Reply};
+use warp::{Filter, Rejection, Reply, body, header, http::StatusCode, path};
 
 /// Creates a new warp [`Filter`] for receiving webhook events.
 ///
